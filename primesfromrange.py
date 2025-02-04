3fe38ed7b3c08d6a13f47adcb572646651691a09
Shrikrishna <rshrikrishna84@gmail.com>
2025-02-04 15:23:48 +0530

    added new files

--- primesfromrange.py
+++ primesfromrange.py
@@ -6,18 +6,18 @@ def prime(num):
             return False
     return True
 
+def digitprime(num):
+    for i in str(num):
+        i=int(i)
+        if not prime(i):
+            return False
+    return True
+
 lower = int(input("Enter lower number: "))
 upper = int(input("Enter upper number: "))
 
 for num in range(lower, upper + 1):
-    if prime(num):
-        print(num, "is a prime number")
+    if prime(num) and digitprime(num):
+        print(num)
 
-        for digit in str(num):
-            digit = int(digit)
-            if prime(digit):
-                print(digit, "is a prime digit of",num)
-            else:
-                print(digit, "is not a prime digit of",num)
-    else:
-        print(num, "is not a prime number")
+
