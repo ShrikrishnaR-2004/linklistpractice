9afe37c0574dba5b47c4a49e54d8bee1f1e385f5
Shrikrishna <rshrikrishna84@gmail.com>
2025-02-05 15:14:19 +0530

    added new files

--- queue.py
+++ queue.py
@@ -1,4 +1,39 @@
-class queue:
-    def __init__(self):
+class Queue:
+    def _init_(self):
         self.queue = []
-    def append(self,):
+
+    def enqueue(self, element):
+        self.queue.append(element)
+
+    def dequeue(self):
+        if self.isEmpty():
+            return "Queue is empty"
+        return self.queue.pop(0)
+
+    def peek(self):
+        if self.isEmpty():
+            return "Queue is empty"
+        return self.queue[0]
+
+    def isEmpty(self):
+        return len(self.queue) == 0
+
+    def size(self):
+        return len(self.queue)
+
+
+# Create a queue
+myQueue = Queue()
+
+myQueue.enqueue('A')
+myQueue.enqueue('B')
+myQueue.enqueue('C')
+print("Queue: ", myQueue.queue)
+
+print("Dequeue: ", myQueue.dequeue())
+
+print("Peek: ", myQueue.peek())
+
+print("isEmpty: ", myQueue.isEmpty())
+
+print("Size: ", myQueue.size())
